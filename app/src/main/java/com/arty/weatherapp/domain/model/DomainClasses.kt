31ad974782ef4e1de4d324679edd5c3d1983f574
mikhaylov97 <27432Domtel>
2018-05-06 18:53:53 +0300

    Chapter 11. Operator overloading.

--- app/src/main/java/com/arty/weatherapp/domain/model/DomainClasses.kt
+++ app/src/main/java/com/arty/weatherapp/domain/model/DomainClasses.kt
@@ -1,4 +1,10 @@
 package com.arty.weatherapp.domain.model
 
-data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
+
+    val size: Int
+        get() = dailyForecast.size
+
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+}
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
